81cda1216c2abf6b82b5e8f9298e3f6b65c91aaa
Garrett Hart <garrett7621@gmail.com>
2017-08-17 20:33:30 -0400

    first commit

---
+++ app/src/main/java/com/example/garretthart/predatorandprey/simulationActivity.java
@@ -0,0 +1,15 @@
+package com.example.garretthart.predatorandprey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class simulationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulation);
+        System.out.println("HELP!");
+        //git update
+    }
+}
